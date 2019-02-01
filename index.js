6daba8f000b005885f56feb6c3b4006a79c7b6c1
adrioro <artistsvillage@me.com>
2019-02-01 10:12:37 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// Try and Catch is similar to if/else, but it looks for an error.
+//Try: code that runs if no errors.
+//Catch: code that runs if there is an error.
+
 try {
     allert("This, the word alert, is typed bad intentionally!. So, it will not alert.")
 } catch (e) // e for error. Could use error instead too.
@@ -10,7 +14,6 @@ try {
 
 
 var Name = prompt("Enter your name:");
-
 try {
     var leng = Name.length; //Length of user input.
     if (leng > 5) {
@@ -23,5 +26,7 @@ try {
 } catch (e) {
     alert(e);
 }
+
+
 
 //If there were a mistake in the try, it'll rund the alert error.
